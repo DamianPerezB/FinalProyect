c1a8f9ca18be62f2e0dbdeb384d5c0fdb8e8e62a
DamianPerezB <89427173+DamianPerezB@users.noreply.github.com>
2021-10-15 17:42:06 -0500

    Update EqualValueCounter.c

--- EqualValueCounter.c
+++ EqualValueCounter.c
@@ -1,6 +1,6 @@
 # define A 16      // Size to array
 # define v 5       // equal values to count in the array
-int array[A] = {1,5,8,2,5,9,7,2,3,8,7,6,3,9,4,1}  //array
+int array[A] = {1,5,8,2,5,9,7,2,3,8,7,6,3,9,4,1};  //array
 
 int count(int c){
   c++;
@@ -12,11 +12,12 @@ int search(int *a, int size, int value){
   for (int i=0; i<size; i++){
     if (value == a[i]){
       finaly = count(finaly);
+    }
   }
   return finaly;
 }
 
-int main ( ) {
+int main (){
   int result = search(array, A, v);
   return 0;
 }
